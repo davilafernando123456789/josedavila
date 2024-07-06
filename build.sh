9cd3175b6f43ecc808c8ed8846198ce0ef43d7e9
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2024-07-06 15:39:48 -0500

    Subeindo app

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
+
+# Crear y activar el entorno virtual de Python
 python3 -m venv .venv
 source .venv/bin/activate
+
+# Actualizar pip e instalar dependencias
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Eliminar cualquier directorio 'public' existente
 rm -rf public
+
+# Inicializar y construir Reflex
 reflex init
 reflex export --frontend-only
+
+# Descomprimir el frontend exportado
 unzip frontend.zip -d public
 rm -f frontend.zip
+
+# Desactivar el entorno virtual
 deactivate
