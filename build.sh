9bcc75014de725d8c2d5083e83b3e8f6049ecd24
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2024-07-06 16:08:16 -0500

    app

--- build.sh
+++ build.sh
@@ -1,40 +1,10 @@
-#!/usr/bin/env bash
-
-# Mostrar la versión de Node.js
-echo "Node version: $(node -v)"
-echo "NPM version: $(npm -v)"
-
-# Crear y activar el entorno virtual de Python
 python3 -m venv .venv
 source .venv/bin/activate
-
-# Actualizar pip e instalar dependencias
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Verificar la instalación de Reflex
-if ! command -v reflex &> /dev/null
-then
-    echo "Reflex no está instalado. Instalando Reflex..."
-    pip install reflex
-fi
-
-# Eliminar cualquier directorio 'public' existente
 rm -rf public
-
-# Inicializar Reflex
-echo "Inicializando Reflex..."
 reflex init
-
-# Construir Reflex
-echo "Construyendo Reflex..."
 reflex export --frontend-only
-
-# Descomprimir el frontend exportado
 unzip frontend.zip -d public
 rm -f frontend.zip
-
-# Desactivar el entorno virtual
 deactivate
-
-echo "Build completo"
